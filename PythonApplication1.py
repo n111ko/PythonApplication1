4ac884bf0ad8d405628a7d05d8da8682267a51f6
niko <niko.aoapp@gmail.com>
2024-12-16 21:10:02 +0200

    a

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,13 @@
-#aaaaaa
+##### Ülesanne 1 #####
+
+a = float(input("Sisesta number: "))
+
+if a > 0:
+    print("See arv on positiivne.")
+    if a % 2:
+        print("See arv on paaris.")
+    else:
+        print("See arv on paaritu.")
+    
+else:
+    print("See arv on negatiivne.")
